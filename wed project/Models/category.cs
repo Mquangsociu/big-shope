241ee23fa19b98e386e716ca90c3140412f0f135
quang <quang@MQUANGSOCIU>
2025-12-15 17:16:07 +0700

    Add project files.

---
+++ wed project/Models/category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace wed_project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Image { get; set; }   // ảnh category
+    }
+}
+
