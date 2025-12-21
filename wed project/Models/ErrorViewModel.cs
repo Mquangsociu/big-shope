ebc435877ed3f9b50f21ee5decc2a3905667d09a
Mquangsociu <quang.pm01236@sinhvien.hoasen.edu.vn>
2025-12-21 23:12:32 +0700

    Delete wed project directory

--- wed project/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace wed_project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
